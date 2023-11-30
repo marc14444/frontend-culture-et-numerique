c71108afb4cce25f023501d614347b3cdde69dea
marc14444 <amanijenmarc41@gmail.com>
2023-11-30 16:31:18 +0000

    cd

--- JAVASCRIPTS/connecter.js
+++ JAVASCRIPTS/connecter.js
@@ -12,5 +12,5 @@ let deconnexion = document.querySelector('.deconnexion');
 deconnexion.addEventListener('click', (e)=>{
     e.preventDefault();
     localStorage.removeItem('session');
-    window.location.href = './connexion.html'
+    window.location.href = './../HTML/connexion.html'
 });
