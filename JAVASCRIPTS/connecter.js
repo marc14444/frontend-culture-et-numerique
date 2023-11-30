e7169460a46f162cabf6558cfe5609c61d4a343f
marc14444 <amanijenmarc41@gmail.com>
2023-11-30 14:01:12 +0000

    gerer deconnexion

---
+++ JAVASCRIPTS/connecter.js
@@ -0,0 +1,19 @@
+const session = localStorage.getItem('session');
+
+const objet = JSON.parse(session)
+
+
+let nom = document.querySelector('.afficherNom');
+
+if(session === null){
+    window.location.href = './connexion.html'
+}else{
+    nom.innerHTML = objet.name
+}
+
+let deconnexion = document.querySelector('.deconnexion');
+deconnexion.addEventListener('click', (e)=>{
+    e.preventDefault;
+    localStorage.removeItem('session');
+    window.location.href = './index.html'
+});
--- JAVASCRIPTS/connexion.js
+++ JAVASCRIPTS/connexion.js
@@ -20,16 +20,21 @@ formulaire.addEventListener('submit', event=>{
     .then(succes =>{
         if(succes.statut == true ){
 
-
           let message = document.querySelector('.messagesDeConnexion');
             message.textContent ='connexion reuissi !';
             message.style.color = 'green';
             message.style.fontSize = "1.5rem";
+            localStorage.setItem('session',JSON.stringify(succes.formData));
             setTimeout(window.location.href = './connecter.html',5000);
         }else{
             message.textContent ='email ou mot de passe incorrecte!';
             message.style.color = 'red';
             message.style.fontSize = "1rem";
         }
-    }) 
+    })
 });
+
+const sessions = localStorage.session;
+if(sessions !== null){
+    window.location.href = './connecter.html';
+}
