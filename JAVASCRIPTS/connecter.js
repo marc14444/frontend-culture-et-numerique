ffa8f6c900bd28cbcb3b232ff24f039b83846375
marc14444 <amanijenmarc41@gmail.com>
2023-11-30 14:43:00 +0000

    kk

--- JAVASCRIPTS/connecter.js
+++ JAVASCRIPTS/connecter.js
@@ -1,16 +1,13 @@
 const session = localStorage.getItem('session');
 
-const objet = JSON.parse(session)
+//const objet = JSON.parse(session)
 
 
 let nom = document.querySelector('.afficherNom');
 
 if(session === null){
     window.location.href = './connexion.html'
-}else{
-    nom.innerHTML = objet.prenom
 }
-
 let deconnexion = document.querySelector('.deconnexion');
 deconnexion.addEventListener('click', (e)=>{
     e.preventDefault();
